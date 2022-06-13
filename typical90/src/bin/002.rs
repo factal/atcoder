0696903e98893e7edc786e0ee7ec46f04ecb8132
factal <joji0425jp@gmail.com>
2022-06-13 18:09:57 +0900

    ac: typical90 002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32
+    }
+
+    for bit in 0..(1 << n) {
+        let mut s = String::new();
+        for i in (0..n).rev() {
+            if bit & (1 << i) != 0 {
+                s.push(')');
+            } else {
+                s.push('(');
+            }
+        }
+        // println!("{}", s);
+
+        let mut cnt = 0;
+        for p in s.chars() {
+            if cnt < 0 {
+                break;
+            }
+            if p == '(' {
+                cnt += 1;
+            } else {
+                cnt -= 1;
+            }
+        }
+
+        if cnt == 0 {
+            println!("{}", s);
+        }
+    }
+
+
 }
