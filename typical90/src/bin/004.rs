b1c932f1bb6bb9c642967be1ac7a78704f212dfe
factal <joji0425jp@gmail.com>
2022-06-14 12:22:06 +0900

    ac: 004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,4 +1,26 @@
-
+use proconio::input;
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [[i32; w]; h]
+    }
+
+    let mut b = vec![0i32;h];
+    let mut c = vec![0i32;w];
+
+    for i in 0..h {
+        for j in 0..w {
+            b[i] += a[i][j];
+            c[j] += a[i][j];
+        }
+    }
+
+    for i in 0..h{
+        let mut l:Vec<String>=vec![];
+        for j in 0..w{
+            l.push((b[i]+c[j]-a[i][j]).to_string());
+        }
+        println!("{}",l.join(" "));
+    }
 }
