bbd5a9c8a578bd9b08c9913e5506adcd71912627
factal <joji0425jp@gmail.com>
2022-06-14 11:46:36 +0900

    rm: unnecessary files

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -1,3 +1,18 @@
+use petgraph::graph;
+use petgraph::visit;
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        e: [(u32, u32); n - 1],
+    };
+
+    let graph = graph::UnGraph::<u32, ()>::from_edges(e);
+    let indices = graph.node_indices();
+
+    let mut bfs = visit::Bfs::new(&graph, start);
+        while let Some(nx) = bfs.next(&graph) {
+        graph[nx] += 1;
+    }
 }
