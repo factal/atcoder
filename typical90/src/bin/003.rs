f43c82c62fc63f0605256b18d8be075b319676be
factal <joji0425jp@gmail.com>
2022-06-14 13:06:03 +0900

    ac: typical90 003

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -1,18 +1,55 @@
-use petgraph::graph;
-use petgraph::visit;
-use proconio::input;
+use proconio::{input,marker::Usize1};
+use std::collections::VecDeque;
 
-fn main() {
-    input! {
-        n: u32,
-        e: [(u32, u32); n - 1],
-    };
+const INF: usize = 1 << 30;
 
-    let graph = graph::UnGraph::<u32, ()>::from_edges(e);
-    let indices = graph.node_indices();
+fn main(){
+    input!{
+        n: usize
+    }
 
-    let mut bfs = visit::Bfs::new(&graph, start);
-        while let Some(nx) = bfs.next(&graph) {
-        graph[nx] += 1;
+    // create adjacency matrix
+    let mut graph = vec![vec![]; n];
+    for _ in 1..n{
+        input!{
+            a: Usize1,
+            b: Usize1
+        }
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let mut dist = vec![INF; n];
+
+    bfs(&graph, &mut dist, 0 as usize);
+
+    let mut u: usize = 0;
+    let max = dist.iter().max().unwrap();
+
+    // get the index of the node farthest from node 0
+    for i in 0..n {
+        if &dist[i] == max {
+            u = i
+        }
+    }
+
+    let mut dist = vec![INF; n];
+    bfs(&graph, &mut dist, u);
+    println!("{}", 1 + dist.iter().max().unwrap());
+}
+
+fn bfs(graph: &Vec<Vec<usize>>, dist: &mut Vec<usize>, s: usize){
+    let mut q: VecDeque<usize> = VecDeque::new();
+    dist[s] = 0;
+    q.push_back(s);
+
+    while q.len() > 0 {
+        let i = q.pop_front().unwrap();
+        for j in graph[i].iter() {
+            if dist[*j] == INF {
+                q.push_back(*j);
+                dist[*j] = dist[i] + 1;
+            }
+        }
     }
 }
