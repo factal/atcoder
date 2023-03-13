df6255b7dfd2093d10224955b5c0517581018621
factal <joji0425jp@gmail.com>
2023-03-14 07:48:34 +0900

    tp011

--- typical90/src/bin/011.rs
+++ typical90/src/bin/011.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        tasks: [(usize, usize, usize); n],
+    }
+
+    let mut tasks = tasks;
+    tasks.sort_by(|a, b| a.0.cmp(&b.0));
+    
+    let mut dp = vec![vec![0; 5001]; n+1];
+
+    for i in 0..n {
+        let (d, c, s) = tasks[i];
+        
+        for j in 0..5001 {
+            if j < c || d < j {
+                dp[i+1][j] = dp[i][j];
+            } else {
+                dp[i+1][j] = std::cmp::max(dp[i][j], dp[i][j-c] + s);
+            }
+        }
+    }
+
+    println!("{}", dp[n].iter().max().unwrap());
 }
